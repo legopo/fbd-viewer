a619722ac946ddac6f3d2936ef8874c378e86230
legopo <taraki3594@aol.jp>
2021-02-14 20:54:09 +0900

    Set font-family

--- src/App.vue
+++ src/App.vue
@@ -26,3 +26,11 @@ export default {
   },
 };
 </script>
+
+<style>
+  #app {
+    font-family: 'Heebo', 'Noto Sans JP', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
